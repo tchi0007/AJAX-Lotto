ab61b52c5ad2ed827cc1166a5d5c4a8fa2c96d04
tchi0007 <tchi0007@algonquinlive.com>
2018-11-02 14:40:13 -0400

    fdojcfd

--- main.js
+++ main.js
@@ -8,7 +8,24 @@ function init() {
 
 }
 
+let url = "https://davidst.edumedia.ca/mad9014/formdata.php";
+
 function btnGenerate(){
     
+    let formdata = new FormData();
+    formdata.append("digits", document.getElementById("digits").value);
+    formdata.append("max", document.getElementById("max").value);
     
+    let customSettings= {
+        method: httpRequest,
+        mode: "cors",
+        body: formdata
+    };
+    
+    let request = new Request(url, customeSettings);
+    
+    fetch(request)
+        .then(function  (response){
+        console.log(response);
+    })
 }
