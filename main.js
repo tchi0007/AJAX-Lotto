f49e88cfcacd473574a8633c3d3970be626f4546
tchi0007 <tchi0007@algonquinlive.com>
2018-11-05 15:24:05 -0500

    buttons

--- main.js
+++ main.js
@@ -1,31 +1,67 @@
 document.addEventListener("DOMContentLoaded", init);
 
+let pages = [];
 
 function init() {
-
     document.getElementById("btnSend").addEventListener("click", btnGenerate);
+
+    document.getElementById("btnBack").addEventListener("click", startAgain);
+
+    pages = document.querySelectorAll(".page");
+    console.log(pages);
 
 
 }
 
-let url = "https://davidst.edumedia.ca/mad9014/formdata.php";
+function startAgain() {
+    pages[0].classList.toggle("active");
+    pages[1].classList.toggle("active");
+}
 
-function btnGenerate(){
-    
-    let formdata = new FormData();
-    formdata.append("digits", document.getElementById("digits").value);
-    formdata.append("max", document.getElementById("max").value);
-    
-    let customSettings= {
-        method: httpRequest,
+function btnGenerate() {
+    pages[0].classList.toggle("active");
+    pages[1].classList.toggle("active");
+
+    let url = "https://davidst.edumedia.ca/mad9014/nums.php";
+    let digits = document.getElementById("digits").value;
+    let max = document.getElementById("max").value;
+
+    let formData = new FormData();
+
+    formData.append("digits", digits);
+    formData.append("max", max);
+
+    let customSettings = {
+        method: "POST",
         mode: "cors",
-        body: formdata
+        body: formData
     };
-    
-    let request = new Request(url, customeSettings);
-    
+
+    let request = new Request(url, customSettings);
+
     fetch(request)
-        .then(function  (response){
-        console.log(response);
-    })
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            console.log(data.numbers);
+
+            let list = document.querySelector(".num_list");
+            list.innerHTML = "";
+
+            for (let item of data.numbers) {
+                let li = document.createElement("li");
+                console.log(item);
+                li.innerHTML = item;
+                list.appendChild(li);
+            }
+
+        })
+        .catch(function (err) {
+            alert("Error: " + err.message);
+
+        });
+
 }
